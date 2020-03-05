dc14e86dd36739f7e4fba8a4d070238f7ae6f826
Сергей Вилков <you@example.com>
2020-03-05 15:59:02 +0300

    Nav menu

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,18 @@ jQuery(function () {
     const $popup          = $('.b-popup');
     const $openPopupBtn   = $('.b-open-popup-btn');
     const $closePopupElem = $('.b-close-popup-elem');
+    const $burger         = $('.b-nav__burger');
+    const $menuArrow      = $('.b-nav__menu-arrow');
+
+    $burger.on('click', function () {
+        $('.b-nav__menu-wrapper').addClass('b-nav__menu-wrapper_state_active');
+    });
+
+
+    $menuArrow.on('click', function () {
+        $('.b-nav__menu-wrapper').removeClass('b-nav__menu-wrapper_state_active');
+    });
+
 
     $openPopupBtn.on('click', function () {
         $popup.addClass('b-popup_state_shown');
