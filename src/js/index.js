721de33925a89c183cc823da45cb0a6ef8bc6cde
Сергей Вилков <you@example.com>
2020-03-05 11:52:20 +0300

    show hide popup logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import jQuery from "jquery";
+
+
+jQuery(function () {
+
+    const $popup          = $('.b-popup');
+    const $openPopupBtn   = $('.b-open-popup-btn');
+    const $closePopupElem = $('.b-close-popup-elem');
+
+    $openPopupBtn.on('click', function () {
+        $popup.addClass('b-popup_state_shown');
+    });
+
+
+    $closePopupElem.on('click', function () {
+        $popup.removeClass('b-popup_state_shown');
+    });
+
+
+});
